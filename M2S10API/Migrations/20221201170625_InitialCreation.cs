cc39a95122847abecafad2afb16161a0cc6f9ed7
LeonardoGasperin <gasperinlvg@gmail.com>
2022-12-01 15:13:50 -0300

    Adicionar arquivos de projeto.

---
+++ M2S10API/Migrations/20221201170625_InitialCreation.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace M2S10API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Requisito = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CargaHorária = table.Column<int>(name: "Carga Horária", type: "int", nullable: true),
+                    Valor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudante",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cpf = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(name: "E-mail", type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    DatadeNascimento = table.Column<DateTime>(name: "Data de Nascimento", type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instructors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(name: "E-mail", type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    ValorHora = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Certificado = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matrícula",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdTurma = table.Column<int>(name: "Id Turma", type: "int", nullable: false),
+                    IdAluno = table.Column<int>(name: "Id Aluno", type: "int", nullable: false),
+                    DataMatricula = table.Column<DateTime>(name: "Data Matricula", type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matrícula", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turma",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdInstrutor = table.Column<int>(name: "Id Instrutor", type: "int", nullable: false),
+                    IdCurso = table.Column<int>(name: "Id Curso", type: "int", nullable: false),
+                    Datadeinício = table.Column<DateTime>(name: "Data de início", type: "datetime2", nullable: true),
+                    Datadetérmino = table.Column<DateTime>(name: "Data de término", type: "datetime2", nullable: true),
+                    CargaHorária = table.Column<int>(name: "Carga Horária", type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turma", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Estudante");
+
+            migrationBuilder.DropTable(
+                name: "Instructors");
+
+            migrationBuilder.DropTable(
+                name: "Matrícula");
+
+            migrationBuilder.DropTable(
+                name: "Turma");
+        }
+    }
+}
